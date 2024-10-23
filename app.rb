959c06688d82420a3ae558ba77b1265fad8b698f
Brian Song <brian.cs.song@gmail.com>
2024-10-23 02:27:27 +0000

    WIP

--- app.rb
+++ app.rb
@@ -3,7 +3,27 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Howdy!</h1>
   <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/rock") do
+  ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  "
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played rock!
+  </h2>
+
+  <h2>
+    We tied!
+  </h2>
   "
 end
